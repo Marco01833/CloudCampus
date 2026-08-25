73141684457a772a96c38768846a490ca1bef5a0
solizperedo05-beep <msolizperedo@gmail.com>
2026-08-24 20:48:02 -0400

    Update header.php

--- header.php
+++ header.php
@@ -45,12 +45,12 @@ if (session_status() !== PHP_SESSION_ACTIVE) {
                        <!-- <li class="nav-item"><a class="nav-link" href="<?= $url_base ?>Permisos/permisos.php">Permisos</a></li> -->
                         <li class="nav-item"><a class="nav-link" href="<?= $url_base ?>sesiones/historial.php">Sesiones activas</a></li> 
                        <li class="nav-item"><a class="nav-link" href="<?= $url_base ?>Cursos/index.php">Cursos</a></li> 
-                       <!-- <li class="nav-item"><a class="nav-link" href="<?= $url_base ?>Contenido/index.php">Contenido</a></li> -->
-                       <!-- <li class="nav-item"><a class="nav-link" href="<?= $url_base ?>Inscripciones/index.php">Inscripciones</a></li> -->
+                       <li class="nav-item"><a class="nav-link" href="<?= $url_base ?>Contenido/index.php">Contenido</a></li>
+                       <!-- <li class="nav-item"><a class="nav-link" href="<?= $url_base ?>Inscripciones/index.php">Inscripciones</a></li> --> 
                     <?php endif; ?>
-                   <!-- <li class="nav-item"><a class="nav-link" href="<?= $url_base ?>Cursos_Usuario/index.php">Mis Cursos</a></li> -->
-                   <!-- <li class="nav-item"><a class="nav-link" href="<?= $url_base ?>Productos/index.php">Cursos</a></li> -->
-                   <!-- <li class="nav-item"><a class="nav-link" href="<?= $url_base ?>Carrito/index.php">Carrito</a></li> -->
+                    <li class="nav-item"><a class="nav-link" href="<?= $url_base ?>Cursos_Usuario/index.php">Mis Cursos</a></li>
+                    <!-- <li class="nav-item"><a class="nav-link" href="<?= $url_base ?>Productos/index.php">Productos</a></li> -->
+                    <!-- <li class="nav-item"><a class="nav-link" href="<?= $url_base ?>Carrito/index.php">Carrito</a></li> -->
                 <?php endif; ?>
             </ul>
             <ul class="navbar-nav ms-auto">
